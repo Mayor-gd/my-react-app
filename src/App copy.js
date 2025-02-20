0d35a841f077d0921e5cc625f0d57e73d48bfdd1
Mayor-gd <r.competente@psp.team>
2025-02-20 11:44:59 +0800

    Initial commit

---
+++ src/App copy.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { useState } from 'react';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import { Box } from '@mui/system';
+
+export default function BasicButtons() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleButtonClick = () => {
+    setIsActive(true);
+  };
+
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        height: '100vh',
+      }}
+    >
+      <Stack spacing={2} direction="row">
+        <Button
+          variant="outlined"
+          onClick={handleButtonClick}
+          sx={{
+            width: '336px',
+            height: '74px',
+            borderRadius: '10px',
+            border: isActive ? '1.5px solid #5850EC' : '1px solid #e4e4e4',
+            textTransform: 'none',
+            fontFamily: 'Nunito, sans-serif',
+            fontWeight: 'bold',
+            fontSize: '16px',
+            color: '#3d3d3d',
+            '&:hover': {
+              backgroundColor: 'rgba(117, 117, 117, 0.06)',
+              border: isActive ? '1.5px solid #5850EC' : '1.5px solid #bdbdbd',
+            },
+          }}
+        >
+          Custom Button
+        </Button>
+      </Stack>
+    </Box>
+  );
+}
